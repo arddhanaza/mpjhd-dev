f5232c6f7096a596f0f22bfe971037893bc54bc3
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2021-02-28 23:27:26 +0700

    fix bug -2

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -10,10 +10,13 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
-    <link rel="stylesheet" href="../assets/vendor/nucleo/css/nucleo.css" type="text/css">
-    <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/assets/vendor/nucleo/css/nucleo.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css')}}"
+          type="text/css">
+    <!-- Page plugins -->
     <!-- Argon CSS -->
-    <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
+    <link rel="stylesheet" href="{{asset('/assets/css/argon.css?v=1.2.0')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('/assets/css/custom.css')}}" type="text/css">
 </head>
 
 <body class="bg-default">
@@ -106,13 +109,14 @@
 </footer>
 <!-- Argon Scripts -->
 <!-- Core -->
-<script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
-<script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../assets/vendor/js-cookie/js.cookie.js"></script>
-<script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-<script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+<script src="{{asset('/assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('/assets/vendor/js-cookie/js.cookie.js')}}"></script>
+<script src="{{asset('/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js')}}"></script>
+<script src="{{asset('/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js')}}"></script>
+
 <!-- Argon JS -->
-<script src="../assets/js/argon.js?v=1.2.0"></script>
+<script src="{{asset('/assets/js/argon.js?v=1.2.0')}}"></script>
 </body>
 
 </html>
